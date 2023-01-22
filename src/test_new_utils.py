da456ad8439d7b5b6bbc0bb3e534025cc3f0f56d
Manav664 <85474549+Manav664@users.noreply.github.com>
2023-01-22 10:17:51 +0530

    updated test_new_utils.py

--- src/test_new_utils.py
+++ src/test_new_utils.py
@@ -119,26 +119,15 @@ def test_stage(file_path):
         df_pre_processed['length'] = df_pre_processed['length'].astype(float)
         df_pre_processed['width'] = df_pre_processed['width'].astype(float)
         df_pre_processed['depth'] = df_pre_processed['depth'].astype(float)
+
     df_pre_processed['ratio'] = round(df_pre_processed['length'] / df_pre_processed['width'],2)
     df_pre_processed['depth %'] = round((df_pre_processed['depth'] / df_pre_processed['width']) * 100,2)
+
     df_pre_processed['cut'] = df_pre_processed.apply(lambda x: post_processing_utils.transform_cut_column(x['cut'],magic_numbers),axis=1)
     df_pre_processed['discount'] = df_pre_processed.apply(lambda x: post_processing_utils.transform_discount_column(x['discount'],magic_numbers,x['price per carat'],x['raprate']),axis=1)
     df_pre_processed['total'] = df_pre_processed.apply(lambda x: post_processing_utils.transform_total_column(x['total'],magic_numbers,x['price per carat'],x['carat']),axis=1)
     df_pre_processed['rap price total'] = df_pre_processed.apply(lambda x: post_processing_utils.transform_rap_total_column(x['rap price total'],magic_numbers,x['raprate'],x['carat']),axis=1)
-    flag = True
-    try:
-        temp = float(df_pre_processed['length'].iloc[0])
-        flag = False
-    except:
-        pass
-    if flag:
-        df_pre_processed['length'],df_pre_processed['width'],df_pre_processed['depth'] = zip(*df_pre_processed['length'].apply(post_processing_utils.transform_measurement_column))
-    else:
-        df_pre_processed['length'] = df_pre_processed['length'].astype(float)
-        df_pre_processed['width'] = df_pre_processed['width'].astype(float)
-        df_pre_processed['depth'] = df_pre_processed['depth'].astype(float)
-    df_pre_processed['ratio'] = round(df_pre_processed['length'] / df_pre_processed['width'],2)
-    df_pre_processed['depth %'] = round((df_pre_processed['depth'] / df_pre_processed['width']) * 100,2)
+
     df_processed=df_pre_processed
 
     # ==== end of all stages ====
